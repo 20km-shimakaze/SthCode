48c0e91d120b951c91e447aa9e4dce7264cab100
acmcode <2801382156@qq.com>
2022-11-30 14:13:09 +0800

    null

--- code/z-test/t6.cpp
+++ code/z-test/t6.cpp
@@ -7,55 +7,46 @@ typedef pair<int,int> P;
 const int N=1e6+7;
 const int INF=0x3f3f3f3f3f3f3f3f;
 const int mod=998244353;
-int n,c,d;
-int a[N],su[N];
-bool check(int k)
-{
-	int sum=su[min(k+1,n)];
-	int num=c/sum;
-	int cc=c%sum;
-	int day=num*(k+1);
-	int dis=lower_bound(su,su+1+n,cc)-su;
-	// cout<<"dis="<<dis<<" "<<day<<" "<<cc<<" "<<sum<<endl;
-	return day+dis<=d;
-}
+map<P,char>mp;
+int idx=0,idx1=0;
 void solve()
 {
-	int maxx=0,summ=0;
-	cin>>n>>c>>d;
-	for(int i=1;i<=n;i++)cin>>a[i];
-	sort(a+1,a+1+n,[&](int a,int b){
-		return a>b;
-	});
-	if(a[1]*d<c){
-		cout<<"Impossible"<<endl;
-		return;
-	}
-	for(int i=1;i<=min(d,n);i++){
-		summ+=a[i];
-	}
-	if(summ>=c){
-		cout<<"Infinity"<<endl;
-		return;
-	}
-	for(int i=1;i<=n;i++)su[i]=su[i-1]+a[i];
-	int l=0,r=1e8+7,ans=0;
-	while(l<=r){
-		int mid=(l+r)/2;
-		if(check(mid)){
-			ans=mid;
-			l=mid+1;
-		}
-		else r=mid-1;
-		// cout<<l<<" "<<r<<endl;
-	}
-	cout<<ans<<endl;
+    int n,m;
+    cin>>n>>m;
+    while(m--){
+        char op;
+        cin>>op;
+        if(op=='+'){
+            int u,v;
+            char c;
+            cin>>u>>v>>c;
+            mp[{u,v}]=c;
+            if(mp.count({v,u})&&mp[{u,v}]==mp[{v,u}])idx++;
+            if(mp.count({v,u}))idx1++;
+        }
+        else if(op=='-'){
+            int u,v;
+            cin>>u>>v;
+            if(mp.count({v,u})&&mp[{v,u}]==mp[{u,v}]){
+                mp.erase({u,v});
+                idx--;
+            }
+            else mp.erase({u,v});
+            if(mp.count({v,u}))idx1--;
+        }
+        else{
+            int k;
+            cin>>k;
+            if((k&1)&&idx1)cout<<"YES"<<endl;
+            else cout<<(idx?"YES":"NO")<<endl;
+        }
+    }
 }
 signed main()
 {
-	//IOS
-	int __=1;
-	cin >> __;
-	while (__--)
-		solve();
+    //IOS
+    int __=1;
+    //cin >> __;
+    while (__--)
+        solve();
 }
